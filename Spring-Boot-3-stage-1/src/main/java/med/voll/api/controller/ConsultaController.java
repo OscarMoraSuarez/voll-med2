38120b0cf84070cd86fdab14827aba4eca4d14dd
RaJSo <yinei505@gmail.com>
2023-10-03 00:37:11 -0500

    hasta implementacion documentacion

--- Spring-Boot-3-stage-1/src/main/java/med/voll/api/controller/ConsultaController.java
+++ Spring-Boot-3-stage-1/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 
 import med.voll.api.domain.consulta.AgendaDeConsultaService;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @ResponseBody
 @RequestMapping("/consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultaController {
 
     @Autowired
